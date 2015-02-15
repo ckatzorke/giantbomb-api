6d7d62d033d4b7b5e20f4e5e77ed0e7adffb1657
Christian Katzorke <c.katzorke@web.de>
2015-02-15 15:32:32 +0100

    added jsdoc

--- src/app/giantbomb.js
+++ src/app/giantbomb.js
@@ -83,13 +83,19 @@ var gbAPI = function (apikey) {
     return {
         /**
          * @name search
-         * @param searchString the query to search for, e.g. Battlefield. Wildcards are not required, and will be interpreted as a search term
          * @desc a simple name based fulltext search
+         * @param searchString the query to search for, e.g. Battlefield. Wildcards are not required, and will be interpreted as a search term
          * @return a Promise for the search, that resolves the JSON object when successful.  The JSON is currently as-is, i.e. as returned from GiantBomb
          */
         'search': function (searchString) {
             return nameSearch(searchString);
         },
+        /**
+         * @name detail
+         * @desc load the game details for given id from GiantBomb and returns the details with all fields provide from GiantBomb
+         * @param id the id for getting the game details from GiantBomb
+         * @return a Promise for the details, that resolves as the JSON object when successful. Only the results field from the api call is returned as SON, containig all fields
+         */
         'detail': function (gameId) {
             return gameDetail(gameId);
         }
