73617934003c867993463ec3a31b732c58264e37
Christian Katzorke <c.katzorke@web.de>
2015-02-15 15:25:56 +0100

    refactored utility responsehandler to prevent DRY. fixed correct promise chaining

--- sample.js
+++ sample.js
@@ -2,8 +2,14 @@
 'use strict';
 var apikey = require('./apikey');
 var gb = require('./src/app/giantbomb.js')(apikey);
-gb.search('Battlefield').then(function (result) {
-    console.log('Found ' + result.number_of_total_results + ' results');
+gb.search('Day of the Tentacle').then(function (result) {
+    var id = result.results[0].id;
+    console.log('Found ' + result.number_of_total_results + ' results (should be 1, loading details for 1st entry). Loading details for id=' + id + '...');
+    return gb.detail(id);
+}).then(function (detail) {
+    console.info('Details for ' + detail.name);
+    console.info('=============================');
+    console.info(detail.deck);
 }).catch(function (error) {
-    console.error(error);
+    console.error("Error during execution chain: ", error);
 });
--- src/app/giantbomb.js
+++ src/app/giantbomb.js
@@ -20,49 +20,72 @@ var gbAPI = function (apikey) {
         fields = {
             'field_list': 'name,id,aliases,genres,image,original_release_date,releases,platforms,api_detail_url,site_detail_url'
         };
+    //util, get the response check for transport errors, and logical errors (from giantbomb in JSON), if an error occured the promise will be rejected, in case of success the JSON object is returned
+    var responseHandler = function (reject, error, response, body) {
+        if (!error) {
+            var result = JSON.parse(body);
+            if (result.error !== 'OK') {
+                reject(new Error(result.error));
+            }
+            return result;
+        } else {
+            console.error("Got error: " + error.message);
+            reject(error); //TODO
+        }
+    };
 
-
-
+    //search for name
     var nameSearch = function (searchString) {
-        console.error('searching for', searchString);
+        console.info('searching for', searchString);
         var searchQS = _.extend(_.clone(qsDefaults), {
                 'filter': 'name:' + searchString,
                 'field_list': 'api_detail_url,id,name,deck,image,original_release_date'
             }),
             searchOptions = _.clone(options);
-        
+
         searchOptions.url = options.url + '/games';
         searchOptions.qs = searchQS;
 
-        console.log('searchOptions', searchOptions);
+        //console.info('searchOptions', searchOptions);
 
         var searchReq = new Promise(function (resolve, reject) {
             request(searchOptions, function (error, response, body) {
-                if (!error) {
-                    var result = JSON.parse(body);
-                    if(result.error !== 'OK'){
-                        reject(new Error(result.error));
-                    }
-                    resolve(result);
-                } else {
-                    console.log("Got error: " + error.message);
-                    reject(error); //TODO
-                }
+                var result = responseHandler(reject, error, response, body);
+                //only need to resolve since rejects are handled by resonsehandler
+                resolve(result);
             });
         });
         return searchReq;
     };
-    
-    var gameDetail = function(gameId){
-        
-    }
+
+    //show details
+    var gameDetail = function (gameId) {
+        console.info('Loading details for id: ' + gameId);
+        var detailOptions = _.clone(options);
+        detailOptions.url = options.url + '/game/3030-' + gameId;
+        detailOptions.qs = qsDefaults;
+        //console.info('detailOptions', detailOptions);
+        var detailsReq = new Promise(function (resolve, reject) {
+            request(detailOptions, function (error, response, body) {
+                var result = responseHandler(reject, error, response, body);
+                //check if a game has been returned
+                if (result.number_of_total_results !== 1) {
+                    reject(new Error('ID ' + gameId + ' seems not to be a valid ID!'));
+                }
+                resolve(result.results);
+            });
+        });
+        return detailsReq;
+    };
+
+
 
     return {
         /**
          * @name search
          * @param searchString the query to search for, e.g. Battlefield. Wildcards are not required, and will be interpreted as a search term
          * @desc a simple name based fulltext search
-         * @return a Promise for the search, that resolves the JSON object when successful
+         * @return a Promise for the search, that resolves the JSON object when successful.  The JSON is currently as-is, i.e. as returned from GiantBomb
          */
         'search': function (searchString) {
             return nameSearch(searchString);
