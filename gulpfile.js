23d5811ee7fbbe3386ad7c06d2e4b1ecb94ec940
Christian Katzorke <c.katzorke@web.de>
2016-12-11 23:28:14 +0100

    runs test now in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
+const mocha = require('gulp-mocha');
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json')
 
-gulp.task('scripts', () => {
+gulp.task('transpile', () => {
   const tsResult = tsProject.src()
-    .pipe(tsProject())
-  return tsResult.js.pipe(gulp.dest('dist'))
-})
+    .pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest('dist'));
+});
 
-gulp.task('watch', ['scripts'], () => {
-  gulp.watch('src/**/*.ts', ['scripts'])
-})
+gulp.task('test', ['transpile'], () => {
+  const test = gulp.src(['dist/test/**/*.test.js'], {
+    read: false
+  }).pipe(mocha({
+    reporter: 'spec'
+  }));
+  return test;
+});
+
+gulp.task('watch', ['test'], () => {
+  gulp.watch('src/**/*.ts', ['test'])
+});
 
 gulp.task('default', ['watch'])
--- src/test/giantbomb.test.ts
+++ src/test/giantbomb.test.ts
@@ -6,22 +6,28 @@ import Giantbomb from '../app/giantbomb';
 const expect = chai.expect;
 
 describe('Test for presence of apikey', () => {
+  const ERROR = 'API key is needed for Giantbomb, please set as environment variable.';
 
   it('should throw an error w/ null api key', () => {
     expect(() => new Giantbomb(null))
-      .to.throw(Error, 'invalid api key');
+      .to.throw(Error, ERROR);
   });
   it('should throw an error w/ undefined api key', () => {
     expect(() => new Giantbomb(undefined))
-      .to.throw(Error, 'invalid api key');
+      .to.throw(Error, ERROR);
   });
   it('should throw an error w/ empty api key', () => {
     expect(() => new Giantbomb(''))
-      .to.throw(Error, 'invalid api key');
+      .to.throw(Error, ERROR);
   });
 
   it('should return a proper instance with api key', () => {
     expect(() => new Giantbomb('abc'))
       .to.not.throw(Error);
   });
+});
+
+
+describe('Test for search', () => {
+
 });
